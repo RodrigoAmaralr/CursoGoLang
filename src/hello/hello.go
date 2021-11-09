a75a23a6ce1698708ef17a5ebb5327350540b8c6
Rodrigo Amaral <rodrigo.com2@gmail.com>
2021-11-09 13:16:32 -0300

    Fazendo requisições para a web

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,25 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	exibeIntrodução()
-	exibeMenu()
-	comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -32,6 +36,7 @@ func exibeIntrodução() {
 }
 
 func exibeMenu() {
+	fmt.Println("\n======MENU======")
 	fmt.Println("1- Inicar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
@@ -42,4 +47,16 @@ func leComando() int {
 	fmt.Scan(&comandoLido)
 	fmt.Println("O valor da variável comando é: ", comandoLido)
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br/"
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site: ", site, " foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site: ", site, " está com problemas. Status Code: ", resp.StatusCode)
+	}
 }
