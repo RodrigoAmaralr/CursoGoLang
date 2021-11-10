b1fbc7e134982436efb19de78e4db94ed89c31be
rodrigo <rodrigo>
2021-11-10 20:14:01 -0300

    adicionando contas

--- src/Go_OrientacaoAObjetos/banco/main.go
+++ src/Go_OrientacaoAObjetos/banco/main.go
@@ -10,10 +10,21 @@ type contaCorrente struct {
 }
 
 func main() {
+	// contaDoRodrigo := contaCorrente{titular: "Rodrigo", numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	// contaDoRabelo := contaCorrente{titular: "Rabelo", saldo: 50.9}
+	// contaDoAmaral := contaCorrente{"Amaral", 222, 111222, 200}
+	// fmt.Println(contaDoRodrigo)
+	// fmt.Println(contaDoRabelo)
+	// fmt.Println(contaDoAmaral)
+
+	// var contaDaCris *contaCorrente
+	// contaDaCris = new(contaCorrente)
+	// contaDaCris.titular = "Cris"
+	// contaDaCris.saldo = 500
+	// fmt.Println(contaDaCris)
+
 	contaDoRodrigo := contaCorrente{titular: "Rodrigo", numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
-	contaDoRabelo := contaCorrente{titular: "Rabelo", saldo: 50.9}
-	contaDoAmaral := contaCorrente{"Amaral", 222, 111222, 200}
-	fmt.Println(contaDoRodrigo)
-	fmt.Println(contaDoRabelo)
-	fmt.Println(contaDoAmaral)
+
+	contaDoRodrigo2 := contaCorrente{titular: "Rodrigo", numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+	fmt.Println(contaDoRodrigo == contaDoRodrigo2)
 }
