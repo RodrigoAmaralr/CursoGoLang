2e7b7a174a30cdd5017e43b46dcc685510fbbbee
rodrigo <rodrigo>
2021-11-11 22:58:59 -0300

    Adicionado a funçao para pagar boleto

--- Go_OrientacaoAObjetos/banco/main.go
+++ Go_OrientacaoAObjetos/banco/main.go
@@ -1,14 +1,14 @@
 package main
 
-/* Comandos necessario para incluir o contaCorrente.go
-PS C:\Users\Rodrigo\go\src\CursoGoLang\Go_OrientacaoAObjetos\banco> go mod init CursoGoLang\Go_OrientacaoAObjetos\banco
-PS C:\Users\Rodrigo\go\src\CursoGoLang\Go_OrientacaoAObjetos\banco>go mod tidy
-*/
-
 import (
 	"CursoGoLang/Go_OrientacaoAObjetos/banco/contas"
 	"fmt"
 )
+
+/* Comandos necessario para incluir o contaCorrente.go
+PS C:\Users\Rodrigo\go\src\CursoGoLang\Go_OrientacaoAObjetos\banco> go mod init CursoGoLang\Go_OrientacaoAObjetos\banco
+PS C:\Users\Rodrigo\go\src\CursoGoLang\Go_OrientacaoAObjetos\banco>go mod tidy
+*/
 
 func main() {
 	// contaDoRodrigo := contaCorrente{}
@@ -45,10 +45,31 @@ func main() {
 	// fmt.Println(contaExemplo.ObterSaldo())
 
 	////////////////
+	// contaDoDenis := contas.ContaPoupanca{}
+	// contaDoDenis.Depositar(100)
+	// contaDoDenis.Sacar(500)
+
+	// fmt.Println(contaDoDenis.ObterSaldo())
+
+	////////////////////
 	contaDoDenis := contas.ContaPoupanca{}
 	contaDoDenis.Depositar(100)
-	contaDoDenis.Sacar(500)
+	PagarBoleto(&contaDoDenis, 60)
 
 	fmt.Println(contaDoDenis.ObterSaldo())
 
+	contaDaLuisa := contas.ContaCorrente{}
+	contaDaLuisa.Depositar(500)
+	PagarBoleto(&contaDaLuisa, 400)
+
+	fmt.Println(contaDaLuisa.ObterSaldo())
+
+}
+
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
 }
